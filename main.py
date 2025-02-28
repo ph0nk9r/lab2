4c83b4523404db740472a819ce2c3298c3b9bb72
Tomen Y <yura.2002.tomen@gmail.com>
2025-02-28 15:22:03 +0300

    Удалены лишние комментарии

--- main.py
+++ main.py
@@ -60,8 +60,7 @@ def search_ipv6_in_text(list_of_word: list) -> list:
     if not isinstance(list_of_word, list):
         raise MyTypeError("list_of_word", list_of_word)
     
-    redex = r"\b(?:[0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}\b"
-    
+    redex = r"[0-9a-f]{0,4}(?:[:.][0-9a-f]{0,4}){7}"
     tandem = []
     for i in list_of_word:
 
